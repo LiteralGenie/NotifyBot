43264968b35352c4f7c38eccae55a2fa95707170
Genie <frothsavd@gmail.com>
2021-01-13 21:51:21 -0500

    ann scraper

--- cogs/update_cog.py
+++ cogs/update_cog.py
@@ -13,24 +13,27 @@ class UpdateCog(commands.Cog, Logger):
 		self.check_times= {}
 		self.iterations= {}
 
-		self.news_channel= self.bot.get_channel(self.bot.config['news_channel'])
-		self.series_channel= self.bot.get_channel(self.bot.config['series_channel'])
 		self.error_channel= self.bot.get_channel(self.bot.config['error_channel'])
 
-		self.get_loop('sushi', SushiScraper).start()
-		self.get_loop('levi', LeviScraper).start()
-		self.get_loop('md', MdScraper).start()
-		self.get_loop('ann', AnnScraper, channel=self.news_channel).start()
 
-	def get_loop(self, name, ScraperClass, channel=None):
-		if channel is None:
-			channel= self.series_channel
+		# zzz, this was prettier before they requested separate channels
+		tmp= lambda x: self.bot.get_channel(self.bot.config[x + '_channel'])
+		self.ann_channel= tmp('ann')
+		self.levi_channel= tmp('levi')
+		self.sushi_channel= tmp('sushi')
+		self.md_channel= tmp('md')
 
+		self.get_loop('sushi', SushiScraper, self.sushi_channel).start()
+		self.get_loop('levi', LeviScraper, self.levi_channel).start()
+		self.get_loop('md', MdScraper, self.md_channel).start()
+		self.get_loop('ann', AnnScraper, self.ann_channel).start()
+
+	def get_loop(self, name, ScraperClass, out_channel):
 		@handle_loop_error(self)
 		@update_check(name, self)
 		async def loop():
 			async for x in (ScraperClass.get_updates()):
-				await channel.send(**x)
+				await out_channel.send(**x)
 
 		kwargs = {
             'seconds': 5, 'minutes': 0, 'hours': 0,
