a1da0634a511dd004b6f53d9efbf2f6b344fd4b0
Genie <frothsavd@gmail.com>
2021-04-14 18:48:52 -0400

    web interface for ping-role settings

---
+++ webview/settings/views.py
@@ -0,0 +1,35 @@
+from quart import views, Response, request
+from mako.template import Template
+from webview import IPC_CLIENT
+from . import LOOKUP, TEMPLATE_DIR
+from ruamel.yaml import YAML
+import utils
+
+# GET for settings summary
+class SettingsView(views.MethodView):
+	async def get(self):
+		bot_config= utils.load_bot_config()
+		role_config= utils.load_yaml(utils.MENTIONS_FILE)
+
+		roles_available= await IPC_CLIENT.request("get_available_roles")
+		series_data= utils.load_json_with_default(utils.SERIES_CACHE, {})
+
+		html= Template(filename=TEMPLATE_DIR + "index.html", lookup=LOOKUP).render(
+			GENERAL=bot_config, ROLES=role_config,
+		  	ROLES_AVAILABLE=roles_available, SERIES_DATA=series_data,
+		)
+
+		return Response(html)
+
+# POST for bot config update
+class GeneralView(views.MethodView):
+	async def post(self):
+		pass
+
+# POST for ping-role update
+class RoleView(views.MethodView):
+	async def post(self):
+		with open(utils.MENTIONS_FILE, "w") as file:
+			print(await request.json)
+			YAML().dump(await request.json, file)
+		return ""
