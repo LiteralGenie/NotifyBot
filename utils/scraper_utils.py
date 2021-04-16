59ee981516707038875286b386151c167f25fe79
Genie <frothsavd@gmail.com>
2021-04-15 23:19:19 -0400

    remove general from template (wip)

--- utils/scraper_utils.py
+++ utils/scraper_utils.py
@@ -5,7 +5,7 @@ def get_session():
 	import aiohttp
 
 	# keep-alive because https://github.com/aio-libs/aiohttp/issues/3904#issuecomment-632661245
-	return aiohttp.ClientSession(headers={'Connection': 'keep-alive', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0'})
+	return aiohttp.ClientSession(headers={'Connection': 'keep-alive', 'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36"})
 
 
 
